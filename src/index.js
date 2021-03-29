67ebf09d6780cde31183f84bb8c28b71d9d85f48
alternateved <alternateved@gmail.com>
2021-03-29 20:59:56 +0200

    Hard-code HTML structure

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 console.log("I am here");
+
+
+/* 
+1. Create animation of striking in CSS/JavaScript
+2. Add functionality to query tasks
+3. Add functionality to segregate task by Today/7day/All
+4. Create logo for page
+5. Display dynamically task in main tag
+6. Find a way to "expand" tasks
+7. Establish color palette
+8. Make page responsive
+9. Properly display elements of the page with CSS
+10. Dynamically create page's content
+*/
