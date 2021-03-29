26adb197d4d8ba2c9e87b044cd31164fc2989718
alternateved <alternateved@gmail.com>
2021-03-29 14:51:23 +0200

    Create basic projectController module

--- src/project.js
+++ src/project.js
@@ -7,12 +7,39 @@ const createTodo = (title, description, dueDate, priority) => {
     }
 }
 
-const createProject = (title) => {
-    let lists = [];
+const projectController = (() => {
+    let projects = [];
 
-    return {
-        title,
-        lists     
+    // check localStorage for data
+    const checkData = () => {
+        // if(localStorage has something) projects = projects localStorage;
+        // else initiateData();
     }
-}
+    
+    const storeData = () => {
+        
+    }
 
+    // initiate default data
+    const initiateData = () => {
+        projects.push(createProject("Default project"));
+    }
+
+    const createProject = (title) => {
+        let lists = [];
+    
+        return {
+            title,
+            lists     
+        }
+    }
+
+    const modifyProject = (index, title) => {
+        projects[index].title = title;
+    }
+
+    const removeProject = (index) => {
+        projects.splice(index, 1);
+    }
+
+})();
