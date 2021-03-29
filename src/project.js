4499255f0e527af923d469c3b32f369c8c45f31f
alternateved <alternateved@gmail.com>
2021-03-29 19:36:27 +0200

    Separate task module

--- src/project.js
+++ src/project.js
@@ -1,6 +1,4 @@
-let projects = [];
-
-const createProject = (title) => {
+const create = (title) => {
   let lists = [];
   return {
     title,
@@ -8,37 +6,12 @@ const createProject = (title) => {
   };
 };
 
-const modifyProject = (index, title) => {
+const modify = (projects, index, title) => {
   projects[index].title = title;
 };
 
-const removeProject = (index) => {
+const remove = (projects, index) => {
   projects.splice(index, 1);
 };
 
-const createTodo = (title, description, dueDate, priority) => {
-  return {
-    title,
-    description,
-    dueDate,
-    priority,
-  };
-};
-
-const modifyTodo = (todo, attribute, newValue) => {
-  todo[attribute] = newValue;
-};
-
-const removeTodo = (projectIndex, todoIndex) => {
-  projects[projectIndex].splice(todoIndex, 1);
-};
-
-export {
-  projects,
-  createProject,
-  modifyProject,
-  removeProject,
-  createTodo,
-  modifyTodo,
-  removeTodo,
-};
+export { create, modify, remove };
---
+++ src/task.js
@@ -0,0 +1,18 @@
+const create = (title, description, dueDate, priority) => {
+  return {
+    title,
+    description,
+    dueDate,
+    priority,
+  };
+};
+
+const modify = (todo, attribute, newValue) => {
+  todo[attribute] = newValue;
+};
+
+const remove = (projects, projectIndex, todoIndex) => {
+  projects[projectIndex].splice(todoIndex, 1);
+};
+
+export { create, modify, remove };
