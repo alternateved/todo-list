12139c20ce641e061e19837983d8594b270c69da
alternateved <alternateved@gmail.com>
2021-04-01 22:35:09 +0200

    Add createMain module

---
+++ src/PageLayout/main.js
@@ -0,0 +1,45 @@
+import createDOM from "./dom";
+
+function createMain() {
+  const main = createDOM("main");
+
+  const tasksContainer = createDOM("div", "tasks-container");
+  main.appendChild(tasksContainer);
+
+  const tasksHeader = createDOM("div", "tasks-header");
+  tasksContainer.appendChild(tasksHeader);
+
+  const headerTitle = createDOM("div", "tasks-title");
+  headerTitle.textContent = "Placeholder";
+  tasksHeader.appendChild(headerTitle);
+
+  const headerSort = createDOM("div", "tasks-sort");
+  tasksHeader.appendChild(headerSort);
+
+  const sortIcon = createDOM("span", "fas", "fa-sort-up");
+  headerSort.appendChild(sortIcon);
+
+  const sortText = createDOM("span");
+  sortText.textContent = "Due date";
+  headerSort.appendChild(sortText);
+
+  const tasksDiv = createDOM("div", "tasks");
+  tasksContainer.appendChild(tasksDiv);
+
+  const addTaskDiv = createDOM("div", "add-task");
+  tasksDiv.appendChild(addTaskDiv);
+
+  const addTaskPanel = createDOM("div", "panel");
+  addTaskDiv.appendChild(addTaskPanel);
+  
+  const addTaskIcon = createDOM("span", "fas", "fa-plus-circle");
+  addTaskPanel.appendChild(addTaskIcon);
+
+  const addTaskText = createDom("span");
+  addTaskText.textContent = "Add task";
+  addTaskPanel.appendChild(addTaskText);
+
+  return main;
+}
+
+export default createMain;
