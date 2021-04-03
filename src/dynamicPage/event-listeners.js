b171f575e931e8de9b750c0dab64a6cbc8b745c7
alternateved <alternateved@gmail.com>
2021-04-03 15:58:11 +0200

    Fix issue with accessing DOM from module

--- src/dynamicPage/event-listeners.js
+++ src/dynamicPage/event-listeners.js
@@ -1,23 +1,22 @@
-import { showProjectBox, hideProjectBox, addNewProject } from "./project-box";
+import projectBox from "./project-box";
 import { showTaskBox } from "./task-box";
 import toggleTheme from "./theme";
 
 function startListening() {
   const addProjectButton = document.querySelector("#add-project");
-addProjectButton.addEventListener("click", showProjectBox);
+  addProjectButton.addEventListener("click", projectBox.show);
 
-const addButton = document.querySelector("#add-project-box");
-addButton.addEventListener("click", addNewProject());
+  const addButton = document.querySelector("#add-project-box");
+  addButton.addEventListener("click", projectBox.addNew);
 
-const cancelButton = document.querySelector("#cancel-project-box");
-cancelButton.addEventListener("click", hideProjectBox);
+  const cancelButton = document.querySelector("#cancel-project-box");
+  cancelButton.addEventListener("click", projectBox.hide);
 
-const addTask = document.querySelector("#add-task");
-addTask.addEventListener("click", showTaskBox());
+  const addTask = document.querySelector("#add-task");
+  addTask.addEventListener("click", showTaskBox());
 
-const themeButton = document.querySelector("#theme-switcher");
-themeButton.addEventListener("click", toggleTheme());
-
+  const themeButton = document.querySelector("#theme-switcher");
+  themeButton.addEventListener("click", toggleTheme());
 }
 
 export default startListening;
--- src/dynamicPage/project-box.js
+++ src/dynamicPage/project-box.js
@@ -1,16 +1,34 @@
-const projectBox = document.querySelector(".project-box");
+const projectBox = (() => {
+  // const projectBox = document.querySelector(".project-box");
+  // const addProjectButton = document.querySelector("#add-project");
+  // console.log(`outside: ${document}, ${projectBox}, ${addProjectButton}`);
+  // console.log(window.document.getElementById("#add-project"));
 
-function showProjectBox() {
-  projectBox.classList.remove("hidden");
-}
-function hideProjectBox() {
-  projectBox.classList.add("hidden");
-}
-function addNewProject() {}
+  const show = () => {
+    const projectBox = document.querySelector(".project-box");
+    const addProjectButton = document.querySelector("#add-project");
+    
+    addProjectButton.classList.add("hidden");
+    projectBox.classList.remove("hidden");
+  };
+  const hide = () => {
+    const projectBox = document.querySelector(".project-box");
+    const addProjectButton = document.querySelector("#add-project");
+    
+    addProjectButton.classList.remove("hidden");
+    projectBox.classList.add("hidden");
+  };
+  const addNew = () => {
+    hide();
+  };
+
+  return { show, hide, addNew };
+})();
+
 //const projectForm = document.querySelector("#project-name");
 //{
 //  console.log(projectForm.value);
 //  projectForm.value = "";
 //}
 
-export { showProjectBox, hideProjectBox, addNewProject };
+export default projectBox;
